0a717e003834bb84a24d480a7fcd63d81d6c7a80
jkc-mycode <jkc9709@naver.com>
2024-04-25 16:10:23 +0900

    Update pagination

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const $movieCards = document.querySelector("#movieCards");
 const $searchContent = document.getElementById("search_content");
 const $searchBtn = document.getElementById("search_btn");
-const movieDataList = [];  // 검색에서 사용할 전역 데이터 리스트
+const $moveTopBtn = document.getElementById("move_top_btn");
+const $moveBottomBtn = document.getElementById("move_bottom_btn");
+let movieDataList = [];  // 검색에서 사용할 전역 데이터 리스트
+
 
 // TMDB API 정보
 const options = {
@@ -12,23 +15,25 @@ const options = {
     }
 };
 
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-    .then(res => res.json())
-    .then(async (res) => {
-        res.results.forEach(item => {
-            appendCard(item.id, item.title, item.overview, item.poster_path, item.vote_average, $movieCards);
-            movieDataList.push(item);  // 검색에서 사용할 전역 데이터 리스트
-        });
-    })
-    .catch(err => console.error(err));
-
+// TMDB에서 페이지번호에 따라 데이터를 가져오는 함수
+const loadData = (pageNum) => {
+    fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${pageNum}`, options)
+        .then(res => res.json())
+        .then(async (res) => {
+            res.results.forEach(item => {
+                appendCard(item.id, item.title, item.overview, item.poster_path, item.vote_average, $movieCards);
+                movieDataList.push(item);  // 검색에서 사용할 전역 데이터 리스트
+            });
+        })
+        .catch(err => console.error(err));
+}
 
 // 매개변수로 받은 영화 정보를 HTML코드에 넣어서 HTML파일에 삽입
 const appendCard = (id, title, overview, posterPath, voteAverage, area) => {
     const html_tmp = `
     <div class="col">
         <div class="card">
-            <img src="https://image.tmdb.org/t/p/w200${posterPath}" class="card-img-top" alt="..." onclick="alert('영화 ID : ${id}')">
+            <img src="https://image.tmdb.org/t/p/original${posterPath}" class="card-img-top" alt="..." onclick="alert('영화 ID : ${id}')">
             <div class="card-body">
                 <h5 class="card-title" style="font-weight: bold;"> ${title} </h5>
                 <br>
@@ -63,6 +68,22 @@ const searchMovie = () => {
 
 // 페이지 로딩 시 실행
 window.onload = () => {
+    // 페이지 로드 시 검색 창에 포커스
+    $searchContent.focus();
+
+    // Top 이동 버튼
+    $moveTopBtn.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+
+    // Bottom 이동 버튼
+    $moveBottomBtn.addEventListener("click", () => {
+        window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+    });
+
+    // 처음 로딩 시 1페이지 출력
+    loadData(1);
+
     // 버튼에 클릭으로 검색 이벤트 추가
     $searchBtn.addEventListener("click", () => {
         searchMovie();
@@ -74,6 +95,20 @@ window.onload = () => {
             searchMovie();
         }
     });
-    
-    $searchContent.focus();
+
+    // 유사 Pagination 구현
+    const $pageValue = document.querySelectorAll(".page-item");
+    const $activeClass = document.getElementsByClassName("active");
+    $pageValue.forEach((item) => {
+        item.addEventListener("click", () => {
+            // 페이지 버튼의 활성화를 위한 코드
+            $activeClass[1].className = $activeClass[1].className.replace(" active", "");
+            item.className += " active";
+
+            $movieCards.replaceChildren();
+            movieDataList = [];
+            loadData(item.value);
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        });
+    });
 }
